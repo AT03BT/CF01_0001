c27da8c52e36bbf3d69b1189de081b9817d2e87a
AT03BT <software@at-bespoke.com>
2025-04-11 02:56:42 +1000

    Add project files.

---
+++ CF01_0001/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CF01_0001.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ CF01_0001/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CF01_0001.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
